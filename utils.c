db977c6941487a879bb423d7f106369bc182a1d0
askchuks <chukwukaokafor1@gmail.com>
2023-09-19 17:04:29 -0700

    utils.c

--- utils.c
+++ utils.c
@@ -15,7 +15,7 @@ return (0);
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ascii_code.
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
